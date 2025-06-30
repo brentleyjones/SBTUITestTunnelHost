a204628d0940d5a8498bff993361f186bbb97d51
Brentley Jones <github@brentleyjones.com>
2025-06-30 13:48:18 -0500

    Add `.linkedFramework`s
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- Package.swift
+++ Package.swift
@@ -17,7 +17,11 @@ let package = Package(
         .target(
             name: "SBTUITestTunnelHost",
             path: "SBTUITestTunnelHost",
-            publicHeadersPath: ""
+            publicHeadersPath: "",
+            linkerSettings: [
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("XCTest"),
+            ]
         ),
     ]
 )
